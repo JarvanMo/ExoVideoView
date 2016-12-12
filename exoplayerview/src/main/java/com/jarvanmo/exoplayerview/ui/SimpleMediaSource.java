f1bfa86e7d7a0d24a14346c3127587bb83c71fd6
jarvanmo <jarvan.mo@gmail.com>
2016-12-12 10:39:09 +0800

    make SimpleMediaSource Pracelable

--- exoplayerview/src/main/java/com/jarvanmo/exoplayerview/ui/SimpleMediaSource.java
+++ exoplayerview/src/main/java/com/jarvanmo/exoplayerview/ui/SimpleMediaSource.java
@@ -1,11 +1,14 @@
 package com.jarvanmo.exoplayerview.ui;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by mo on 16-11-30.
  * this package is com.jarvanmo.exoplayerview.ui
  */
 
-public class SimpleMediaSource {
+public class SimpleMediaSource implements Parcelable {
 
     private String displayName;
 
@@ -27,4 +30,31 @@ public class SimpleMediaSource {
         return url;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.displayName);
+        dest.writeString(this.url);
+    }
+
+    protected SimpleMediaSource(Parcel in) {
+        this.displayName = in.readString();
+        this.url = in.readString();
+    }
+
+    public static final Parcelable.Creator<SimpleMediaSource> CREATOR = new Parcelable.Creator<SimpleMediaSource>() {
+        @Override
+        public SimpleMediaSource createFromParcel(Parcel source) {
+            return new SimpleMediaSource(source);
+        }
+
+        @Override
+        public SimpleMediaSource[] newArray(int size) {
+            return new SimpleMediaSource[size];
+        }
+    };
 }
