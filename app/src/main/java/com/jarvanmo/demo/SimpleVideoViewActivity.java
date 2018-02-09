2f467e637670a0ba72d2ba1baab4a89d293ff619
JarvanMo <jarvan.mo@gmail.com>
2018-02-09 22:36:56 +0800

    simplevideoviewActivity

--- app/src/main/java/com/jarvanmo/demo/SimpleVideoViewActivity.java
+++ app/src/main/java/com/jarvanmo/demo/SimpleVideoViewActivity.java
@@ -1,12 +1,9 @@
 package com.jarvanmo.demo;
 
-import android.graphics.Color;
 import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -14,45 +11,28 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
-import com.jarvanmo.exoplayerview.media.ExoMediaSource;
 import com.jarvanmo.exoplayerview.media.SimpleMediaSource;
-import com.jarvanmo.exoplayerview.media.SimpleQuality;
 import com.jarvanmo.exoplayerview.ui.ExoVideoView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.jarvanmo.exoplayerview.orientation.OnOrientationChangedListener.SENSOR_LANDSCAPE;
 import static com.jarvanmo.exoplayerview.orientation.OnOrientationChangedListener.SENSOR_PORTRAIT;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SimpleVideoViewActivity extends AppCompatActivity {
 
     private ExoVideoView videoView;
-    private Button modeFit;
-    private Button modeNone;
-    private Button modeHeight;
-    private Button modeWidth;
-    private Button modeZoom;
     private View wrapper;
-    private Button play;
-    private View contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        contentView = findViewById(R.id.activity_main);
-
+        setContentView(R.layout.activity_simple_video_view);
         videoView = findViewById(R.id.videoView);
-        modeFit = findViewById(R.id.mode_fit);
-        modeNone = findViewById(R.id.mode_none);
-        modeHeight = findViewById(R.id.mode_height);
-        modeWidth = findViewById(R.id.mode_width);
-        modeZoom = findViewById(R.id.mode_zoom);
+        Button modeFit = findViewById(R.id.mode_fit);
+        Button modeNone = findViewById(R.id.mode_none);
+        Button modeHeight = findViewById(R.id.mode_height);
+        Button modeWidth = findViewById(R.id.mode_width);
+        Button modeZoom = findViewById(R.id.mode_zoom);
         wrapper = findViewById(R.id.wrapper);
-        play = findViewById(R.id.changeMode);
 
         videoView.setBackListener((view, isPortrait) -> {
             if (isPortrait) {
@@ -72,42 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
 //
 
-//       final SimpleMediaSource mediaSource = new SimpleMediaSource("http://video19.ifeng.com/video07/2013/11/11/281708-102-007-1138.mp4");
-//        SimpleMediaSource mediaSource  = new SimpleMediaSource("https://tungsten.aaplimg.com/VOD/bipbop_adv_example_v2/master.m3u8");
-//        final SimpleMediaSource mediaSource = new SimpleMediaSource("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8");
-//        SimpleMediaSource mediaSource  = new SimpleMediaSource(" https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear0/prog_index.m3u8");
-//       SimpleMediaSource mediaSource = new SimpleMediaSource("https://tungsten.aaplimg.com/VOD/bipbop_adv_fmp4_example/master.m3u8");
-//        SimpleMediaSource mediaSource = new SimpleMediaSource("http://pullhlsbb8f2e48.live.126.net/live/7de213ebb3dc4db2aa2f32f3da0b028d/playlist.m3u8");
-//        SimpleMediaSource mediaSource = new SimpleMediaSource("http://rotation.vod.zlive.cc/channel/1234.m3u8");
-//        SimpleMediaSource mediaSource = new SimpleMediaSource("https://media.w3.org/2010/05/sintel/trailer.mp4");
-        SimpleMediaSource mediaSource = new SimpleMediaSource("http://flv2.bn.netease.com/videolib3/1604/28/fVobI0704/SD/fVobI0704-mobile.mp4");
-//        SimpleMediaSource mediaSource = new SimpleMediaSource("http://stream1.hnntv.cn/lywsgq/sd/live.m3u8");
+        SimpleMediaSource mediaSource  = new SimpleMediaSource(" http://playready.directtaps.net/smoothstreaming/SSWSS720H264PR/SuperSpeedway_720.ism");
 
-        mediaSource.setDisplayName("VideoPlaying");
-        List<ExoMediaSource.Quality> qualities = new ArrayList<>();
-        ExoMediaSource.Quality quality;
-
-        for (int i = 0; i <6 ; i++) {
-            SpannableString spannableString  = new SpannableString("Quality"+i);
-            if(i % 2 ==0){
-                ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.YELLOW);
-                spannableString.setSpan(colorSpan, 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-
-            }else {
-                ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.RED);
-                spannableString.setSpan(colorSpan, 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-            }
-
-            quality = new SimpleQuality(spannableString,mediaSource.url());
-            qualities.add(quality);
-        }
-        mediaSource.setQualities(qualities);
+        mediaSource.setDisplayName("Super speed");
 
         videoView.play(mediaSource,false);
-        play.setOnClickListener(view -> {
-            videoView.play(mediaSource);
-            play.setVisibility(View.INVISIBLE);
-        });
 
 
         modeFit.setOnClickListener(v -> videoView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT));
@@ -119,16 +68,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void changeToPortrait() {
 
+        // WindowManager operation is not necessary
         WindowManager.LayoutParams attr = getWindow().getAttributes();
 //        attr.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Window window = getWindow();
         window.setAttributes(attr);
         window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
+
         wrapper.setVisibility(View.VISIBLE);
     }
 
 
     private void changeToLandscape() {
+
+        // WindowManager operation is not necessary
+
         WindowManager.LayoutParams lp = getWindow().getAttributes();
 //        lp.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
         Window window = getWindow();
